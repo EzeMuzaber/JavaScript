d362c3b9b60c3c201f358cafcdbdf2cab0af5c02
ezequiel muzaber <ezequielmuzaber@gmail.com>
2023-10-02 10:59:18 -0300

    primer commit de javascript

---
+++ JavaScript/main.js
@@ -0,0 +1,54 @@
+// comentario de linea
+/*
+comentario de bloque
+*/
+/*  */
+//let edad = 29; //una variable tipo number
+//let nombre= "Ezequiel";//variable tipo string
+
+//no se puede cambiar el valor de la constante
+//const IVA = 0.21;
+
+/* nombre = "Alejandro";
+
+console.log("hola ", nombre)
+console.log("cuantos años tenes?", "tengo: " ,edad) */
+
+/* let numeroUno= 5;
+let numeroDos=10;
+
+let resultadoSuma= numeroUno + numeroDos;
+let resultadoResta= numeroUno - numeroDos;
+let resultadoMultiplicacion= numeroUno * numeroDos;
+let resultadoDivision= numeroUno / numeroDos;
+
+
+console.log("la suma es: ",resultadoSuma);
+console.log("la resta es: ",resultadoResta);
+console.log("la multiplicacion es: ",resultadoMultiplicacion);
+console.log("la division es: ",resultadoDivision); */
+/* 
+let saludo = "hola";
+let nombre1 = "ezequiel";
+
+let saludoCompleto =saludo + " " + nombre1 + " "+ "!!!";
+console.log(saludoCompleto); */
+
+
+/* let nombre2= prompt("ingrese su nombre");
+alert ("hola " + nombre2);
+ */
+
+/* let numero3= Number(prompt("ingrese un numero"));
+alert (numero3 +4);
+  */
+let nombre = prompt("ingresa tu nombre");
+alert ("hola" +" "+  nombre + "," + " que tengas un lindo dia!!");
+
+let edad = Number(prompt("ingresa tu edad: "));
+alert ("Mi edad es:" + " " + edad);
+
+let entrada = prompt(" ingresa tu nombre");
+let salida = prompt ("ingresa tu apellido");
+alert("Mi nombre completo es: " + " " + entrada + " " + salida);
+
