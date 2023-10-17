d5aab51330d8777c8a7bb15fee37105fad0fa353
ezequiel muzaber <ezequielmuzaber@gmail.com>
2023-10-16 21:18:08 -0300

    se agrego hasta mitad de la clase 7, de lo visto.

--- JavaScript/ejemplos/ejemplos.js
+++ JavaScript/ejemplos/ejemplos.js
@@ -113,10 +113,12 @@ valor1 || valor2 es verdadero cuando al menos 1 de los valores es verdadero.
 //   alert("Datos incorrectos");
 // }
 
-// function saludar(){
+
+ //***************CLASE 4*****************************
+ //FUNCIONES:
+ // function saludar(){
 //   console.log("Hola");
 // }
-
 // saludar();
 
 // function pedirDatos(){
@@ -125,7 +127,41 @@ valor1 || valor2 es verdadero cuando al menos 1 de los valores es verdadero.
 // }
 // pedirDatos();
 
-//  EJEMPLO DE CALCULADORA
+ // ************FUNCION CON PARAMETRO:****************
+
+/* function saludar (saludo,nombre){
+  console.log(`${saludo} ${nombre}`)
+}
+saludar("hola ", "ezequiel"); */
+
+/* function enviarCorreo(correo){
+  alert (`correo enviado a ${correo}`);
+}
+
+let correo = prompt("ingrese su correo para enviar la informacion: ");
+enviarCorreo(correo);
+ */
+
+/* ****************EJEMPLO MUÑECO************************* */
+
+/* function armar(material){
+  return `muñeco armado con  ${material}`;
+}
+
+function pintar(munecoArmado){
+  return `${munecoArmado} y pintado`;
+}
+function ropaBodega(munecoPintado){
+  console.log (`${munecoPintado} vestido y enviado a bodega`);
+}
+
+let armado = armar ("madera");
+let pintado = pintar (armado);
+ropaBodega(pintado);  */
+
+/* *******FIN EJEMPLO MUÑECO*************** */
+
+/* *************** EJEMPLO DE CALCULADORA***************** */ 
 
 // function calculadora(num1, num2, operacion) {
 //   switch (operacion) {
@@ -159,7 +195,267 @@ valor1 || valor2 es verdadero cuando al menos 1 de los valores es verdadero.
 // let resultado = calculadora(numeroUno, numeroDos, operacion);
 
 // alert(`${numeroUno} ${operacion} ${numeroDos} = ${resultado}`);
+// --------------------FIN EJEMPLO CALCULADORA--------------------------
 
+/* *********************ARROW FUNCTION***************************** */
+
+/* const suma =(num1,num2) => {
+  return num1 + num2;
+}
+console.log (suma(5,10)); */
+
+/* const saludar = nombre => {
+  return `hola ${nombre}`;
+}
+console.log (saludar("camila")); */
+
+/* const sumar = (num1,num2) => num1 + num2;
+
+console.log(sumar (5,10));
+ */
+/* 
+const suma = (a,b) => a + b;
+const resta = (a,b) => a - b;
+const iva = (precio) => precio * 0.21;
+
+let precioProducto = 1000;
+let descuento = 100; */
+
+/* let precioProducto= prompt("ingrese el precio del producto");
+let descuento= prompt ("ingrese el descuento"); */
+
+
+// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)),descuento);
+            /*   = resta(suma(1000, iva(1000)),100)
+                  = resta (suma(1000,210)100)
+                  =resta(1210,100)
+                  =1110 */
+//console.log(nuevoPrecio);
+
+/* ********************ARRAYS*********************** */
+//const nombres = ["eze","ali","alex","anto","ciro"];
+
+/* console.log(nombres[2]); */
+/* for(let index=0;index < 5; index++){
+  console.log(nombres[index]);
+} */
+
+//const productos=["camisa","pantalon","gorra"];
+
+/* console.log(productos);
+console.log(productos.indexOf("camison")); */
+
+/* ******PARA ELIMINAR UN NOMBRE DEL ARREGLO *********** */
+
+/* const eliminarPorNombre = (nombre) => {
+  let index = nombres.indexOf(nombre);
+
+  if (index != -1){
+    nombres.splice(index, 1);
+    console.log(nombres);
+  }
+} 
+eliminarPorNombre("ciro");
+ */
+
+
+/* ****************** FOR OF *********************** */
+
+/* const producto = [
+  { id:1 , nombre: "camisa", precio: 1000 },
+  { id:2 , nombre: "gorra", precio: 750 },
+  { id:3 , nombre: "zapato", precio: 377 },
+  { id:4 , nombre: "media", precio: 150 },
+]; */
+
+/* for(const item of producto){
+  console.log(producto);
+  console.log(item.id);
+  console.log(item.nombre);
+  console.log(item.precio);
+}
+ */
+
+// OTRA FORMA DE HACERLO:
+
+/* class producto {
+  constructor (nombre,precio){
+    this.nombre = nombre;
+    this.precio=precio;
+  }
+}
+
+const productos = [];
+productos.push(new producto("camisa", 1000));
+productos.push(new producto("gorra", 700));
+productos.push(new producto("media", 350));
+productos.push(new producto("pantalon", 150));
+
+console.log(productos);
+ */
+
+/* ****************** CLASE 7 ***************************** */
+
+/*  function mayorQue(n){
+  return (m) => m > n;
+}
+
+let mayorQueDiez = mayorQue(10); // (m) => m > 10;
+console.log(mayorQueDiez(15)); // (15) => 15 > 10;
+console.log(mayorQueDiez(5)); */
+
+/* ******* EJEMPLO DE UN CARRO DE OPERACIONES ************ */
+
+/* function operaciones(op){
+  if(op === "sumar"){
+    return (x, y) => x + y;
+  }
+  if(op === "restar"){
+    return (x, y) => x - y;
+  }
+  if(op === "multiplicar"){
+    return (x, y) => x * y;
+  }
+  if(op === "dividir"){
+    return (x, y) => x / y;
+  }
+}
+
+let sumar = operaciones ("sumar"); // (x,y) => x + y;
+console.log(sumar(9,1));
+let restar = operaciones ("restar"); // (x,y) => x - y;
+console.log(restar(20,12));
+let multiplicar = operaciones ("multiplicar"); // (x,y) => x * y;
+console.log(multiplicar(5,5));
+let dividir = operaciones ("dividir"); // (x,y) => x / y;
+console.log(dividir(10,2)); */
+
+/* ************** FIN EJEMPLO CARRO DE OPERACIONES ****************** */
+
+/* function iterador(arreglo , funcion){
+  for(const numero of arreglo){
+    funcion(numero);
+    //console.log(numero);
+  }
+}
+ iterador([33,4,5,9], console.log);
+ */
+//-----------------
+/*  function  mostrar(numero){
+  console.log(`Hola soy ${numero}`);
+}
+
+function iterador(arreglo , funcion){
+  for(const numero of arreglo){
+  mostrar (numero);
+  }
+}
+ iterador([33,4,5,9], mostrar)
+ */
+/* *******LO MISMO PERO CON NOMBRE *****************/
+/*  function  mostrar(nombre){
+  console.log(`Hola soy ${nombre}`);
+}
+
+function iterador(arreglo , funcion){
+  for(const nombre of arreglo){
+  mostrar (nombre);
+  }
+}
+ iterador(["alex","eze","antoo","walooo"], mostrar); */
+
+//---------------------------------------------
+
+const productos = [
+  { id:1 , nombre: "camisa", precio: 1000 },
+  { id:2 , nombre: "gorra", precio: 750 },
+  { id:3 , nombre: "zapato", precio: 377 },
+  { id:4 , nombre: "media", precio: 150 },
+];
+
+//metodo forEach
+
+/* productos.forEach(item => {
+  console.log(item.nombre);
+}); */
+
+
+//metodo Find:
+
+/* const encontrado = productos.find((item) => item.nombre ==="camisa");
+console.log(encontrado);
+ */
+//otro metodo:
+
+/* let nombre = prompt("ingrese el nombre del producto: ");
+
+const producto = productos.find((item) => item.nombre === nombre);
+
+if(producto){
+  alert(`
+    nombre: ${producto.nombre}
+    precio: ${producto.precio}
+  `);
+}else{
+  alert ("producto no encontrado")
+};
+ */
+ 
+//metodo filter:
+
+/* const filtrados = productos.filter((item) => item.precio > 500);
+console.log(filtrados); */
+
+//OTRA FORMA DE HACERLO:
+
+let precio = Number(prompt("ingrese el precio minimo: "));
+
+const filtrados =productos.filter((item) => item.precio > precio);
+
+filtrados.forEach((producto) => {
+  alert(`
+    Nombre: ${producto.nombre}
+    Precio: ${producto.precio}
+    `);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+/* ***************EJEMPLO ROPA ******************** */
 /* let opcion= prompt(`
 ingrese la opcion requerida:
 
@@ -194,11 +490,20 @@ ingrese la opcion requerida:
   3. Ropa de niño.
   `);
 } */
+/* ************************FIN EJEMPLO ROPÁ *************************** */
 
 
 
 
 
+
+
+
+
+
+/* ********************TRABAJO 1 PARTES-******************************* */
+
+/* 
 while (opcion === undefined|| opcion.toLocaleLowerCase() !== "salir") {
   opcion = prompt(`
   ingrese su edad:
@@ -347,3 +652,4 @@ Juego elegido: ${juegoElegido};
 Dificultad: ${nivelDificultad};
 
 `);
+ */
